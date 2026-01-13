83e4ffeac6e6c8d24f8c39c3e61efa244f7b8c74
AMM-M <adasmaciejewski09@gmail.com>
2026-01-13 19:16:15 +0100

    obrut prtawie zrobiony

--- ClumsyCoyote/Assets/Scrips/rotetionOfPlatform.cs
+++ ClumsyCoyote/Assets/Scrips/rotetionOfPlatform.cs
@@ -4,23 +4,41 @@ public class rotetionOfPlatform : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Transform platforma;
-    public Transform rotacja;
+    public Transform rotacja1;
+    public Transform rotacja2;
     public float moveSpeed = 2f;
-    private bool Move = false;
+    // public Collider2D Triger2;
+    private bool Move1 = false;
+    private bool Move2 = false;
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Move = true;
+        Move1 = true;
+        Move2 = false;
     }
+    // void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     Move2 = true;
+    //     Move1 = false;
+    // }
     void Update()
     {
-        if (Move == true)
+        if (Move1 == true)
         {
-            if (platforma.rotation != rotacja.rotation)
+            if (platforma.rotation != rotacja1.rotation)
             {
-                platforma.rotation = Quaternion.RotateTowards (platforma.rotation, rotacja.rotation, moveSpeed  * Time.deltaTime);
+                platforma.rotation = Quaternion.RotateTowards (platforma.rotation, rotacja1.rotation, moveSpeed  * Time.deltaTime);
             } else {
-                Move = false;
+                Move1 = false;
+            }
+        }
+        if (Move2 == true)
+        {
+            if (platforma.rotation != rotacja2.rotation)
+            {
+                platforma.rotation = Quaternion.RotateTowards (platforma.rotation, rotacja2.rotation, moveSpeed  * Time.deltaTime);
+            } else {
+                Move2 = false;
             }
         } 
     }
